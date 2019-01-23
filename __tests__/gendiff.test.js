d8b54c0cb0404bbb49b1658fd9bf696902f16f99
Grigoriy Levanov <glevanov@gmail.com>
2019-01-23 20:20:44 +0300

    Add result reading from fs in test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,9 @@
 import gendiff from '../src';
+import fs from 'fs';
 
-const before = '';
-const after = '';
-const result = '';
+const before = '__tests__/__fixtures__/before.json';
+const after = '__tests__/__fixtures__/after.json';
+const result = fs.readFileSync('__tests__/__fixtures__/result', 'utf8');
 
 test('gendiff compares JSON', () => {
   expect(gendiff(before, after)).toBe(result);
