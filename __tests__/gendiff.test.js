16b09980cc98fa3027d81b090291f79d6b566ca4
Grigoriy Levanov <glevanov@gmail.com>
2019-01-24 22:07:59 +0300

    Fix second task review comments

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,10 +1,10 @@
 import fs from 'fs';
 import gendiff from '../src';
 
-const before = '__tests__/__fixtures__/before.json';
-const after = '__tests__/__fixtures__/after.json';
-const result = fs.readFileSync('__tests__/__fixtures__/result', 'utf8');
-
 test('gendiff compares JSON', () => {
+  const before = '__tests__/__fixtures__/before.json';
+  const after = '__tests__/__fixtures__/after.json';
+  const result = fs.readFileSync('__tests__/__fixtures__/result', 'utf8');
+
   expect(gendiff(before, after)).toBe(result);
 });
--- src/index.js
+++ src/index.js
@@ -8,30 +8,21 @@ const gendiff = (firstConfig, secondConfig) => {
   const firstFile = readJSON(firstConfig);
   const secondFile = readJSON(secondConfig);
   const keys = _.union(Object.keys(firstFile), Object.keys(secondFile));
-  const acc = [];
-  const getComparisonString = (key) => {
+
+  const result = keys.reduce((acc, key) => {
     if (_.has(firstFile, key) && _.has(secondFile, key)) {
       if (firstFile[key] === secondFile[key]) {
-        return `    ${key}: ${firstFile[key]}`;
+        return `${acc}\n    ${key}: ${firstFile[key]}`;
       }
-      return `  + ${key}: ${secondFile[key]}`
-        + '\n'
-        + `  - ${key}: ${firstFile[key]}`;
+      return `${acc}\n  + ${key}: ${secondFile[key]}\n  - ${key}: ${firstFile[key]}`;
     }
     if (_.has(firstFile, key) && !_.has(secondFile, key)) {
-      return `  - ${key}: ${firstFile[key]}`;
+      return `${acc}\n  - ${key}: ${firstFile[key]}`;
     }
-    if (!_.has(firstFile, key) && _.has(secondFile, key)) {
-      return `  + ${key}: ${secondFile[key]}`;
-    }
-    return '';
-  };
+    return `${acc}\n  + ${key}: ${secondFile[key]}`;
+  }, '');
 
-  keys.forEach(it => acc.push(getComparisonString(it)));
-  acc.unshift('{');
-  acc.push('}');
-
-  return `${acc.join('\n')}\n`;
+  return `{${result}\n}\n`;
 };
 
 export default gendiff;
