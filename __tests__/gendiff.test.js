8aff800c513611e66acaf0c867be887c8c49584b
Grigoriy Levanov <glevanov@gmail.com>
2019-01-24 22:36:15 +0300

    Add yaml test case

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,17 @@
 import fs from 'fs';
 import gendiff from '../src';
 
-test('gendiff compares JSON', () => {
+test('JSON comparison', () => {
   const before = '__tests__/__fixtures__/before.json';
   const after = '__tests__/__fixtures__/after.json';
+  const result = fs.readFileSync('__tests__/__fixtures__/result', 'utf8');
+
+  expect(gendiff(before, after)).toBe(result);
+});
+
+test('YML comparison', () => {
+  const before = '__tests__/__fixtures__/before.yml';
+  const after = '__tests__/__fixtures__/after.yml';
   const result = fs.readFileSync('__tests__/__fixtures__/result', 'utf8');
 
   expect(gendiff(before, after)).toBe(result);
